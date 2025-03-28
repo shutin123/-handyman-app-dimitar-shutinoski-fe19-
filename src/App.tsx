f9718fec41ddd65165512747f3960c31f2ff187f
Dimitar Shutinoski <dimitarshutinbt@gmail.com>
2025-03-28 11:58:51 +0100

    /

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,6 @@
 import "./App.css";
-import BottomNavigationHandymen from "../src/BottomNavigationBar/BottomNavigationHandymen/BottomNavigationHandymen";
 import "./styles/typography.css";
 
-function App() {
-  return (
-    <>
-      <BottomNavigationHandymen />
-    </>
-  );
-}
+function App() {}
 
 export default App;
--- src/BottomNavigationBar/BottomNavigationHandymen/BottomNavigationHandymen.tsx
+++ src/BottomNavigationBar/BottomNavigationHandymen/BottomNavigationHandymen.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import FooterImage from "../../layouts/FooterImage/FooterImage";
 import NavigationMenu from "../../layouts/NavigationMenu/HandyMen/component/NavigationMenu";
-import Onboarding from "../../components/onboarding/Onboarding";
 
 const BottomNavigationHandymen: React.FC = () => {
   return (
     <>
       <div className="BottomNavigationBar">
-        <Onboarding />
         <NavigationMenu />
         <FooterImage />
       </div>
