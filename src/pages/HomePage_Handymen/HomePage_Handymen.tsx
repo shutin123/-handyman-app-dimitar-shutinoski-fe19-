293a880bec125eff94d57328e28dc8371136deaf
Dimitar Shutinoski <dimitarshutinbt@gmail.com>
2025-04-14 15:58:11 +0200

    Done for categories and value propositions components of handymen homepage UI, later adding functionality and etc

--- src/pages/HomePage_Handymen/HomePage_Handymen.tsx
+++ src/pages/HomePage_Handymen/HomePage_Handymen.tsx
@@ -2,14 +2,21 @@ import React from "react";
 import StatusBar from "../../layouts/StatusBar/StatusBar";
 import Footer from "../../other/footer/Footer";
 import Hero from "../../components/homepage_handyman/Hero/Hero";
+import ValueProposition from "../../components/homepage_handyman/ValueProposition/ValuePropostion";
+import Categories from "../../components/homepage_handyman/Categories/Categories";
+import "./HomePage_Handymen.css";
 
 const HomePageHandymen: React.FC = () => {
   return (
-    <>
-      <StatusBar />
-      <Hero />
+    <div className="homepage-handymen">
+      <div className="top-section">
+        <StatusBar />
+        <Hero />
+      </div>
+      <ValueProposition />
+      <Categories />
       <Footer />
-    </>
+    </div>
   );
 };
 
